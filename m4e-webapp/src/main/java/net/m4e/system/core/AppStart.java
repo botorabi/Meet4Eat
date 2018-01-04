e08de479b592df6372669093afe2a0f3c76b9e53
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-01-04 15:04:31 +0100

    Add/fix License-Header

--- m4e-webapp/src/main/java/net/m4e/system/core/AppStart.java
+++ m4e-webapp/src/main/java/net/m4e/system/core/AppStart.java
@@ -1,11 +1,10 @@
-/**
+/*
  * Copyright (c) 2017-2018 by Botorabi. All rights reserved.
  * https://github.com/botorabi/Meet4Eat
- * <p>
+ *
  * License: MIT License (MIT), read the LICENSE text in
- * main directory for more details.
+ *          main directory for more details.
  */
-
 package net.m4e.system.core;
 
 import javax.ws.rs.core.Application;
