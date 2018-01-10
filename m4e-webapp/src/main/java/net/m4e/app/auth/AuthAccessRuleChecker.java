0120049b2b4df536c5417a8f87ca3cda1c237f7c
Botorabi <botorabi@gmx.net>
2018-01-10 11:44:45 +0100

    Added unit-tests for AuthAccessChecker

--- m4e-webapp/src/main/java/net/m4e/app/auth/AuthAccessRuleChecker.java
+++ m4e-webapp/src/main/java/net/m4e/app/auth/AuthAccessRuleChecker.java
@@ -8,11 +8,16 @@
 
 package net.m4e.app.auth;
 
-import java.lang.invoke.MethodHandles;
-import java.util.*;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 
 /**
@@ -20,7 +25,7 @@ import org.slf4j.LoggerFactory;
  * access against those rules.
  * 
  * @author boto
- * Date of creation 23.08.2017
+ * Date of creation August 23, 2017
  */
 public class AuthAccessRuleChecker {
 
@@ -51,7 +56,7 @@ public class AuthAccessRuleChecker {
      */
     public AuthAccessRuleChecker(String path) {
         resourcePath = path;
-        resourcePathRegexp = resourcePath.replaceAll("\\{([^}]+)\\}", "(.+)");
+        resourcePathRegexp = createPathRegExp(path);
         accessRules = new HashMap<>();
     }
 
@@ -66,9 +71,9 @@ public class AuthAccessRuleChecker {
 
     /**
      * Get the resource path as regular expression so far any placeholder {} were
-     * found in resource path.
+     * found in resource path. If the path was invalid, then null will be returned.
      * 
-     * @return  Resource path as regular expression
+     * @return  Resource path as regular expression, or null if path was invalid
      */
     public String getResourcePathRegexp() {
         return resourcePathRegexp;
@@ -91,6 +96,9 @@ public class AuthAccessRuleChecker {
      * @return              Return true if successful, if the role already exists then return false.
      */
     public boolean addAccessRoles(String accessMethod, List<String> accessRoles) {
+        if ((accessMethod == null) || (accessRoles == null)){
+            return false;
+        }
         List<String /*role*/> roles = accessRules.get(accessMethod);
         if (roles == null) {
             roles = new ArrayList<>();
@@ -109,6 +117,9 @@ public class AuthAccessRuleChecker {
      * @return                  Return true if authorization is granted, otherwise false
      */
     public boolean checkFixPath(String path, String accessMethod, List<String> userRoles) {
+        if ((path == null) || (accessMethod == null)){
+            return false;
+        }
         if (!path.contentEquals(resourcePath)) {
             return false;
         }
@@ -125,34 +136,52 @@ public class AuthAccessRuleChecker {
      * @return                  Return true if authorization is granted, otherwise false
      */
     public boolean checkComplexPath(String complexPath, String accessMethod, List<String> userRoles) {
-        try {
-            if (!complexPath.matches(resourcePathRegexp)) {
-                return false;
-            }
-        }
-        catch(Exception ex) {
-            /*TODO: possible Exceptions are: NullPointerException and PatternSyntaxException
-                   NPE shouldn't be handled like this.
-                   PSE should be handled at construction-time (fail-fast)
-            */
-            LOGGER.error("Error while checking the resource path: " + ex.getLocalizedMessage());
+        if ((complexPath == null) || (accessMethod == null)) {
             return false;
         }
+        if (!complexPath.matches(resourcePathRegexp)) {
+            return false;
+        }
+        return checkRoles(accessMethod, userRoles);
+    }
 
-        return checkRoles(accessMethod, userRoles);        
+    /**
+     * Try to create a proper regular expression for later pattern matching. If the path is not complex
+     * then the passed string is returned. If a complex path was malformed then null is returned.
+     *
+     * @param regularExpression The path to create a regular expression for
+     * @return                  The reg exp for the path, or null if the path was malformed
+     */
+    protected String createPathRegExp(String regularExpression) {
+        String regexp = regularExpression.replaceAll("\\{([^}]+)\\}", "(.+)");
+        if (regexp.contains("{") || regexp.contains("}")) {
+            LOGGER.error("Bad access rule checker path detected: " + regularExpression);
+            return null;
+        }
+
+        try {
+            Pattern.compile(regexp);
+        } catch (PatternSyntaxException ex) {
+            LOGGER.error("Bad access rule checker path detected: " + ex.getLocalizedMessage());
+            return null;
+        }
+        return regexp;
     }
 
     /**
      * Given an access method (such as GET, POST, etc.) check if any of user roles
      * match to required ones.
      * 
-     * NOTE: Access to special role AuthRole.NO_CHECK are always granted.
+     * NOTE: Access to paths with roles AuthRole.NO_CHECK or AuthRole.VIRT_ROLE_GUEST are always granted.
      * 
      * @param accessMethod  Access method
      * @param userRoles     List of user roles
      * @return              Return true if at least one role matched, otherwise false.
      */
-    private boolean checkRoles(String accessMethod, List<String> userRoles) {
+    protected boolean checkRoles(String accessMethod, List<String> userRoles) {
+        if ((accessMethod == null) || (userRoles == null)) {
+            return false;
+        }
         List<String /*role*/> roles = accessRules.get(accessMethod);
         if (roles == null) {
             return false;
