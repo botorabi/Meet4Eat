4f03d6fca0e66fdb823c2a3c4420f648c03e8ac5
Yannick Bröker <ybroeker@techfak.uni-bielefeld.de>
2018-01-12 20:00:25 +0100

    Add Headers...

--- m4e-webapp/src/main/java/net/m4e/app/mailbox/cmds/MailOperationCmd.java
+++ m4e-webapp/src/main/java/net/m4e/app/mailbox/cmds/MailOperationCmd.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2017-2018 by Botorabi. All rights reserved.
+ * https://github.com/botorabi/Meet4Eat
+ *
+ * License: MIT License (MIT), read the LICENSE text in
+ *          main directory for more details.
+ */
 package net.m4e.app.mailbox.cmds;
 
 import net.m4e.app.mailbox.MailOperation;
