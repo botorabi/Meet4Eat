a68276837259294952c99b5dd19e6c1f972e73ef
Unknown <a.botorabi@avm.de>
2018-01-19 16:17:59 +0100

    Fixed an issue for MS Windows and system try

--- m4e-client-desktop/src/app/gui/systemtray.cpp
+++ m4e-client-desktop/src/app/gui/systemtray.cpp
@@ -77,6 +77,7 @@ void SystemTray::onActivated( QSystemTrayIcon::ActivationReason reason )
     {
         case QSystemTrayIcon::Trigger:
         case QSystemTrayIcon::DoubleClick:
+            showIconNotify( false );
             common::GuiUtils::bringWidgetToFront( _p_mainWindow );
         break;
 
